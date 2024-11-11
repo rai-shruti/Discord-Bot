8592d5adb4b6de1717b92be1693bfb834cce985b
rai shruti <raishruti100305@gmail.com>
2024-11-11 23:06:06 +0530

    Remove sensitive information from code

--- command.js
+++ command.js
@@ -12,7 +12,7 @@ const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 try {
     console.log('Started refreshing application (/) commands.');
   
-   // await rest.put(Routes.applicationCommands('1304771036272660531'), { body: commands });
+    await rest.put(Routes.applicationCommands(process.env.DISCORD_APP_ID), { body: commands });
   
     console.log('Successfully reloaded application (/) commands.');
   } catch (error) {
